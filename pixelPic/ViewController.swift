9ee2deedd1d12547faab33bd4c619b94fc8415a8
陳彥佑 <apulu@apulude-macbook-air.local>
2018-06-15 16:56:38 +0800

    Initial Commit

---
+++ pixelPic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pixelPic
+//
+//  Created by APULU on 2018/6/15.
+//  Copyright © 2018 APULU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
